c456a3442302ded5eff6237bc2b6540942fdf904
ahmedsaleh17 <ahmedsalehsh@gmail.com>
2026-05-13 14:51:43 +0300

    add problem

---
+++ ProblemSolving/11.py
@@ -0,0 +1,11 @@
+def weakest_strong_link(strength):
+	"""
+	This fucntion take an matrix and return weakest strong chain link 
+	weakest strong link is the minimum number in it's row and maxium number in it's column 
+	
+	Args: 
+        strength (list): list of list contains numbers 
+	Return: 
+        interger number 
+	"""
+	return -1
